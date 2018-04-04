b35c751fce1f9d6a678468917af389acda4679dc
韩继瑶 <hanjiyao0@gmail.com>
2018-04-04 17:05:01 +0800

    update

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -7,21 +7,21 @@ import ImgReliable from './Images/reliable.png'
 class AboutUs extends Component {
     render() {
         return (
-            <section className="about-us">
-                <div className="about-wrap container">
-                    <div className="row">
-                        <div className="col-sm p-5">
-                            <img class="img-fluid" src={ImgAbout} height="200" />
+            <section className="about-us bg-primary">
+                <div className="about-wrap p-0 container text-white">
+                    <div className="row about-row pt-md-5">
+                        <div className="col-md text-center p-3 m-auto">
+                            <img id="about_logo" src={ImgAbout} />
                         </div>
-                        <div className="col-sm">
-                            <div className="about-title p-3">
+                        <div className="col-md pl-5 pr-5 m-auto">
+                            <div className="about-title pt-md-5">
                                 <h1>
                                     Seaview Transport
                                 </h1>
                                 <div className="title-sep">
                                 </div>
                             </div>
-                            <div className="about-txt">
+                            <div className="about-txt pb-md-5">
                                 <p>
                                     Seaview Transport started in 1993, handling the separation and recycling of waste 
                                     (e.g. waste, plastic, stones, papers, steel and scrap metals).
@@ -32,29 +32,19 @@ class AboutUs extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="getQuotes">
-                    <div className="card text-white bg-primary m-3">
-                        <div className="card-body">
-                            <h5 className="card-title">Act Now</h5>
-                            <p className="card-text">We cater to your needs for a personal service, let us know your prefered time , location and other requests</p>
-                            <div className="text-right">
-                                <a href="#" className="btn btn-light">Get a Quotes</a>
+                <div className="features">
+                    <div className="card-warp p-md-5 bg-white">
+                        <div className="feature-card card-group text-center pt-5">
+                            <div className="card">
+                                <img className="card-img-top" src={ImgFlexible} alt="flexible" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Flexible</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            <div className="features">
-                <div className="card-group ml-3 mr-3 text-center">
-                    <div className="card">
-                            <img className="card-img-top" src={ImgFlexible} alt="flexible" />
-                            <div className="card-body">
-                                <h5 className="card-title">Flexible</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <img className="card-img-top" src={ImgReliable} alt="reliable" />
+                            <div className="card">
+                                <img className="card-img-top" src={ImgReliable} alt="reliable" />
                                 <div className="card-body">
                                     <h5 className="card-title">Reliable</h5>
                                     <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -63,14 +53,26 @@ class AboutUs extends Component {
                             </div>
                             <div className="card">
                                 <img className="card-img-top" src={ImgAvailable} alt="available" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">24 hr Available</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                <div className="card-body">
+                                    <h5 className="card-title">24 hr Available</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="get-quotes">
+                            <div className="card m-5">
+                                <div className="card-body bg-primary text-white">
+                                    <h5 className="card-title">Act Now</h5>
+                                    <p className="card-text">We cater to your needs for a personal service, let us know your prefered time , location and other requests</p>
+                                    <div className="text-right">
+                                        <a href="#" className="btn btn-light">Get a Quotes</a>
                                     </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
             </section>
         )
     }
