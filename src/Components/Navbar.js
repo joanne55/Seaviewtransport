6b7ffb626290edfd152f5945ae2a30ce4022b31e
Joanne <170801C@mymail.nyp.edu.sg>
2018-04-09 21:01:01 +0800

    Test push

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,22 +9,25 @@ class Navbar extends Component {
 
   render() {
     return (
-      <div className="no-padding">
-        <nav className="row navbar navbar-expand-sm container-fluid bg-light">
+      <div>
+        <nav className="row navbar navbar-expand container-fluid navbar-light bg-light">
           <img className="col-sm-2 img-responsive" src={Logo} />
           <h2 className="col-sm-3">Seaview Transport</h2>
           <div className="col-sm-3"></div>
-          <ul className="col-sm-4 row navbar-nav nav-tabs navbar-right">
-            <li className="col-sm-4 nav-item">
+
+          <div className="collapse navbar-collapse navbar-toggler">
+          <ul className="col-sm-4 navbar-nav nav-tabs">
+            <li className= "nav-item active">
               <a className="nav-link" href="#">About us</a>
             </li>
-            <li className="col-sm-4 nav-item">
+            <li className= "nav-item">
               <a className="nav-link" href="#">Services</a>
             </li>
-            <li className="col-sm-4 nav-item">
+            <li className= "nav-item">
               <a className="nav-link" href="#">Contact us</a>
             </li>
           </ul>
+          </div>
         </nav>
       </div>
     )
